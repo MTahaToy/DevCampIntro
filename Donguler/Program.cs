acbb34cae8b8114a0824175ae4ac54dd1350c1c8
Mehmet Taha Toy <mttoy45@gmail.com>
2021-01-17 00:42:05 +0300

    Proje dosyası ekle.

---
+++ Donguler/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kurs1 = "Yazılım gelişitirici kampı";
+            string kurs2 = "Programlamaya baslangic icin temel kurs";
+
+            string[] kurslar = new string[] { "Yazılım gelişitirici kampı" , "Programlamaya baslangic icin temel kurs" };
+
+            for (int i = 0; i < kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+            Console.WriteLine("For bitti");
+            foreach (string kurs in kurslar)//dizilere uygulanır
+            {
+                Console.WriteLine(kurs);
+            }
+
+            Console.WriteLine("Sayfa sonu");
+        }
+    }
+}
---
+++ KampIntro/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //type safety - tip güvenliği
+            string kategoriEtiketi = "Kategori";
+            int ogrencisayisi = 32000;
+            bool sistemeGirisYapmisMi = true;
+            double dolarDun = 7.35;
+            double dolarBugun = 7.45;
+            if (dolarDun>dolarBugun)
+            {
+                Console.WriteLine("Azalis butonu");
+            }
+            else if (dolarDun<dolarBugun)
+            {
+                Console.WriteLine("Artis butonu");
+            }
+            else
+            {
+                Console.WriteLine("Degismedi butonu");
+            }
+
+
+            if (sistemeGirisYapmisMi==true)
+            {
+                Console.WriteLine("Kullanıcı ayarları butonu");
+            }
+            else
+            {
+                Console.WriteLine(kategoriEtiketi);
+            }
+           
+        }
+    }
+}
